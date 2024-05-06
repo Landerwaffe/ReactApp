5c2900fea910ff8890057c28835c1c4c7945df33
Landerwaffe <jonathan.daniel@connect.qut.edu.au>
2024-05-07 01:51:03 +0700

    Conditional Rendering
    Display a list if an array is more than 0, else display notification text

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -12,12 +12,26 @@ class Counter extends Component {
     fontWeight: "bold",
   };
 
+  renderTags() {
+    if (this.state.tags.length == 0) return <p>There are no tags!</p>;
+
+    return (
+      <ul>
+        {this.state.tags.map((tag) => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
+    );
+  }
+
   render() {
     return (
       <React.Fragment>
         {/* <img src={this.state.imageUrl} alt="" /> */}
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button className="btn btn-secondary btn-sm">Increment</button>
+        {this.state.tags.length == 0 && "Please Create a new tag!"}
+        {this.renderTags()}
       </React.Fragment>
     );
   }
