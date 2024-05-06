ea8a4b23284ea394adfd9b2dee9fee2fb033e69b
Landerwaffe <jonathan.daniel@connect.qut.edu.au>
2024-05-07 00:59:29 +0700

    GetBadgeClasses()
    Method for changing class of element based on its state

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,20 +3,34 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0,
+    imageUrl: "https://picsum.photos/200",
+    tags: ["tag1", "tag2", "tag3"],
+  };
+
+  styles = {
+    fontSize: 10,
+    fontWeight: "bold",
   };
 
   render() {
     return (
       <React.Fragment>
-        <span>{this.formatCount()}</span>
-        <button>Increment</button>
+        {/* <img src={this.state.imageUrl} alt="" /> */}
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
       </React.Fragment>
     );
   }
 
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count == 0 ? "warning" : "primary";
+    return classes;
+  }
+
   formatCount() {
     const { count } = this.state;
-    return count == 0 ? <h1>Zero</h1> : count;
+    return count == 0 ? "Zero" : count;
   }
 }
 
